de2ead260487c9bda06e5101d90e6a4bc9bb96f7
Meepster Hooves <42324391+Meepsterkh@users.noreply.github.com>
2019-05-11 21:06:53 -0700

    Update Pichu.py

--- Pichu.py
+++ Pichu.py
@@ -5,6 +5,7 @@ import discord
 import random
 import requests
 import os
+import time
 
 
 # def main settings
@@ -45,31 +46,38 @@ class messageFun():
 class SmashFn():
     def __init__(self, message):
         self.message = message
+        self.died = False
 
     async def reset(self):
         for i in range(100):
             for i in self.message.server.roles:
-                if(str(i)[0] == "~"):
+                if(str(i)[-1] == "%"):
                     await client.delete_role(self.message.server, i)
 
-        await client.create_role(self.message.server, name = "~0%")
-        rolePercent = discord.utils.get(self.message.server.roles, name='~0%')
+        await client.create_role(self.message.server, name = "0%")
+        rolePercent = discord.utils.get(self.message.server.roles, name='0%')
         for member in self.message.server.members:
             await client.add_roles(member, rolePercent)
-        await client.send_message(self.message.channel, "Game has been Reset, Fight")
+        await client.send_message(self.message.channel, "3!")
+        time.sleep(.5)
+        await client.send_message(self.message.channel, "2!")
+        time.sleep(.5)
+        await client.send_message(self.message.channel, "1!")
+        time.sleep(.5)
+        await client.send_message(self.message.channel, "GO!")
 
-    async def attack(self, percentChange: int):
+    async def attack(self, percentChange: int, growth: int, base: int):
         for member in self.message.server.members:
             if (str(self.message.content[-19:-1]) == str(member.id)):
                 roles = discord.utils.get(self.message.server.members, name=member.name).roles
                 for i in roles:
                     roleSpill = str(i).split()
-                    if(roleSpill[0][0] == "~"):
-                        percent = int(roleSpill[0][1:-1])
+                    if(roleSpill[0][-1] == "%"):
+                        percent = int(roleSpill[0][:-1])
                         await client.remove_roles(member, i)
 
 
-                        numStr = "~" + str(percent + percentChange) + "%"
+                        numStr = str(percent + percentChange) + "%"
                         does = False
                         for i in self.message.server.roles:
                             if(i != numStr):
@@ -77,11 +85,29 @@ class SmashFn():
                         if(does):
                             await client.create_role(self.message.server, name=numStr)
 
+                        await client.wait_until_ready()
                         rolePercent = discord.utils.get(self.message.server.roles, name=numStr)
                         await client.add_roles(member, rolePercent)
-                        await client.send_message(self.message.channel, str(member.name) + " Now Has " + numStr)
 
+                        await self.check(member, percent + percentChange, growth, base)
+                        if(self.died == False):
+                            await client.send_message(self.message.channel, str(member.name) + " now has " + numStr)
+                            self.died = False
 
+    async def check(self, member, percent, growth, base):
+        total = (percent * growth) + (base * 25)
+
+        if(total >= 7500):
+            await client.send_message(self.message.channel, str(member.name) + " died at " + str(percent) + "%")
+            rolePercent = discord.utils.get(self.message.server.roles, name=str(percent) + "%")
+            await client.remove_roles(member, rolePercent)
+            rolePercent = discord.utils.get(self.message.server.roles, name='0%')
+            await client.add_roles(member, rolePercent)
+            self.died = True
+
+#turn[0], 3 stocks
+
+#total Knockback == (percent * knockback growth) + base knockback
 
 @client.event
 async def on_message(message):
@@ -115,46 +141,11 @@ async def on_message(message):
     if(str(message.author) == "Itchymitchy11#5914"):
         await client.send_message(message.channel, "THUNDERBOLT")
 
+
     if (message.content.startswith("|reset")):
         await Smash.reset()
 
-    # if(message.content.startswith("|reset")):
-    #     for i in range(100):
-    #         for i in message.server.roles:
-    #             if(str(i)[0] == "~"):
-    #                 await client.delete_role(message.server, i)
-    #
-    #     await client.create_role(message.server, name = "~0%")
-    #     rolePercent = discord.utils.get(message.server.roles, name='~0%')
-    #     for member in message.server.members:
-    #         await client.add_roles(member, rolePercent)
-
     if (message.content.startswith("|falcon punch")):
-        await Smash.attack(27)
-
-    # if(message.content.startswith("|falcon punch")):
-    #     for member in message.server.members:
-    #         if (str(message.content[16:-1]) == str(member.id)):
-    #             roles = discord.utils.get(message.server.members, name=member.name).roles
-    #             for i in roles:
-    #                 roleSpill = str(i).split()
-    #                 if(roleSpill[0][0] == "~"):
-    #                     percent = int(roleSpill[0][1:-1])
-    #                     await client.remove_roles(member, i)
-    #
-    #
-    #                     numStr = "~" + str(percent + 27) + "%"
-    #                     does = False
-    #                     for i in message.server.roles:
-    #                         if(i != numStr):
-    #                             does = True
-    #                     if(does):
-    #                         await client.create_role(message.server, name=numStr)
-    #
-    #                     rolePercent = discord.utils.get(message.server.roles, name=numStr)
-    #                     await client.add_roles(member, rolePercent)
-
-
-
+        await Smash.attack(27, 100, 1)
 
 client.run(str(os.environ.get('BOT_TOKEN')))
